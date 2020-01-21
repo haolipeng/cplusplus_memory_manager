c2d78b0feccff6d1ea30b0f6f01e6435760ac173
haolipeng <1078285863@qq.com>
2020-01-21 15:12:44 +0800

    上次忘记提交cmake文件

---
+++ cmake-build-debug/CMakeFiles/cplusplus_memory_manager.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cplusplus_memory_manager.dir/main.cpp.obj"
+  "CMakeFiles/cplusplus_memory_manager.dir/perClassAllocator.cpp.obj"
+  "cplusplus_memory_manager.pdb"
+  "cplusplus_memory_manager.exe"
+  "cplusplus_memory_manager.exe.manifest"
+  "libcplusplus_memory_manager.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cplusplus_memory_manager.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
